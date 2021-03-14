f30aa82f09d5de1547b6610e48ef9162eb1a0a97
bruna01freitas <bruna.freitas@furg.br>
2021-03-14 19:43:04 -0300

    create

--- desafio_bonus/consultas.sql
+++ desafio_bonus/consultas.sql
@@ -1,6 +1,6 @@
 --consultas a serem realizadas no banco
 --E.B.3 Faça uma querie que traga todos os navers ordenados pelo seu tempo de empresa admission_date
-select cliente from clientes
+select navers from navers
 order by admission_date
 --E.B.4 Faça uma querie que traga todos os projetos com seus respectivos navers.
 
--- desafio_bonus/inserIntoClientes.sql
+++
@@ -1,6 +0,0 @@
-TRUNCATE TABLE navers;
-
-INSERT INTO clientes(nome, job_role,birthdate,admission_date,) VALUES ('Juliana', 'analista de dados',22/09/2003, 20/04/2018)
-INSERT INTO clientes(nome, job_role,birthdate,admission_date,) VALUES ('Maria', 'analista de marketing',09/09/2000, 02/09/2020);
-INSERT INTO clientes(nome, job_role,birthdate,admission_date,) VALUES ('João', 'desenvolvedor front-end',05/02/1995, 12/11/2019);
-INSERT INTO clientes(nome, job_role,birthdate,admission_date,) VALUES ('Fernanda', 'desenvolvedor mobile',15/04/1997, 23/01/2018);
